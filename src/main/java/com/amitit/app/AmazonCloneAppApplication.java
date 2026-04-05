57a8185947fb6281e8e005a73d228f3640a3eb8c
Abhishek Tatale <tataleabhishek@gmail.com>
2026-04-05 14:56:01 +0530

    Initial commit with project folders

---
+++ src/main/java/com/amitit/app/AmazonCloneAppApplication.java
@@ -0,0 +1,13 @@
+package com.amitit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonCloneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonCloneAppApplication.class, args);
+	}
+
+}
