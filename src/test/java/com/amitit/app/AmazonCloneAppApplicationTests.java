2bf2fdf8e12b896e423dacca0e2117487abc365f
Abhishek Tatale <tataleabhishek@gmail.com>
2026-04-05 15:19:50 +0530

    sec commit

---
+++ src/test/java/com/amitit/app/AmazonCloneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amitit.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonCloneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
